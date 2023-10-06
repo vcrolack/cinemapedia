d4a66ab913268bcfbb331b0f745f6b047e15dd4b
Victor Rolack <victorolack@gmail.com>
2023-10-06 15:28:01 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# cinemapedia
+
+## Dev
+
+1. Copiar .env.template y renombrarlo a .env
+2. Cambiar las variables de entorno
---
+++ lib/config/router/app_router.dart
@@ -0,0 +1,10 @@
+import 'package:cinemapedia/presentation/screens/screens.dart';
+import 'package:go_router/go_router.dart';
+
+final appRouter = GoRouter(initialLocation: '/', routes: [
+  GoRoute(
+    name: HomeScreen.name,
+    path: '/',
+    builder: (context, state) => const HomeScreen(),
+  )
+]);
